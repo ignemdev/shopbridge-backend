d4378ef01451349104f9646095134ddb0747f22a
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2023-03-18 09:44:19 -0400

    needed packages

--- src/ShopBridge.Api/Program.cs
+++ src/ShopBridge.Api/Program.cs
@@ -1,25 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace ShopBridge.Api;
+public static class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        /*se instancia la clase y se le pasa la propiedad
+        del builder que implementa IConfiguration*/
+        var startup = new Startup(builder.Configuration);
+        /*Se configuran los servicios de Startup con los
+        servicios del builder*/
+        startup.ConfigureServices(builder.Services);
+        /*Se genera la app*/
+        var app = builder.Build();
+        /*Se pasa la aplicacion y el enviroment de la misma*/
+        startup.Configure(app, app.Environment);
+        /*Se corre la aplicacion*/
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ src/ShopBridge.Api/Startup.cs
@@ -0,0 +1,46 @@
+﻿namespace ShopBridge.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        /// <summary>
+        /// Donde se configuran los servicios
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        /// <summary>
+        /// Donde se configuran los middlewares y el request pipeline
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
